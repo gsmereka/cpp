d4a2967f3036f3d63ca91a7fd35cc6fb6dcf82e8
gsmereka <gsmereka200@gmail.com>
2024-04-25 22:16:59 -0300

    logic-ok

--- cpp01/ex04/sources/main.cpp
+++ cpp01/ex04/sources/main.cpp
@@ -5,24 +5,24 @@ static void	replace(std::ifstream &input, std::ofstream &output, std::string con
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
-	{
+	if (argc != 4){
 		std::cerr << "Usage: " << argv[0] << " <filename> <S1> <S2>" << std::endl;
 		return 1;
 	}
+
 	std::ifstream input(argv[1]);
-	if (!input.is_open())
-	{
+	if (!input.is_open()){
 		std::cerr << "Unable to open file:" << argv[1] << std::endl;
 		return (1);
 	}
-	std::ofstream output(std::string(argv[1]) + ".replace");
-	if (!output.is_open())
-	{
+
+    std::ofstream output((std::string(argv[1]) + ".replace").c_str());
+	if (!output.is_open()){
 		input.close();
 		std::cerr << "Unable to create output file" << std::endl;
 		return (1);
 	}
+
 	replace(input, output, argv[2], argv[3]);
 	input.close();
 	output.close();
