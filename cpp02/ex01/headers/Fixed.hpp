ddb0f02026abc9156b5d0b09078775b9e71fbd0d
gsmereka <gsmereka200@gmail.com>
2024-05-23 20:08:13 -0300

    set-stream-overload-and-to_int-to_float

---
+++ cpp02/ex01/headers/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(const Fixed &other);
+		Fixed(const int integer);
+		Fixed(const float f);
+		~Fixed(void);
+		Fixed& operator=(const Fixed &other);
+		float	toFloat (void) const;
+		int		toInt (void) const;
+		int		getRawBits(void) const;
+		void	setRawBits(const int raw);
+	private:
+		int _value;
+		static const int _fractionalBits = 8;
+};
+
+std::ostream &operator<<(std::ostream &outfile, Fixed const &instance);
+
+#endif
---
+++ cpp02/ex01/sources/Fixed.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "../headers/Fixed.hpp"
+
+// CONSTRUCTORS AND DESTRUCTOR
+Fixed::Fixed(void) 
+{
+	_value = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->operator=(other);
+}
+
+Fixed &Fixed::operator = (const Fixed &obj)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &obj)
+	{
+		this->_value = obj.getRawBits();
+	}
+	return (*this);
+}
+
+Fixed::Fixed(const int integer)
+{
+	this->_value = integer << _fractionalBits;
+}
+
+Fixed::Fixed(const float f)
+{
+	this->_value = (int)round(f * (1 << Fixed::_fractionalBits));
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+// METHODS
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _value;
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	this->_value = raw;
+}
+
+float	Fixed::toFloat (void) const
+{
+	return ((float)this->_value / (float) (1 << Fixed::_fractionalBits));
+}
+
+int	Fixed::toInt (void) const
+{
+	return (this->_value >> Fixed::_fractionalBits);
+}
+
+// STREAM OVERLOADS
+std::ostream & operator<<(std::ostream & outfile, const Fixed & fixed)
+{
+    outfile << fixed.toFloat();
+    return outfile;
+}
