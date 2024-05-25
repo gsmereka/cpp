16f7b0ea3528b9c05a07de88f4a790746253601c
gsmereka <gsmereka200@gmail.com>
2024-05-24 22:10:08 -0300

    att-main

--- cpp03/ex01/sources/main.cpp
+++ cpp03/ex01/sources/main.cpp
@@ -2,7 +2,7 @@
 #include "../headers/ScavTrap.hpp"
 #include <iostream>
 
-int main()
+int main2()
 {
     ClapTrap ct("Clappy");
     ct.attack("target");
@@ -10,6 +10,37 @@ int main()
     ct.beRepaired(3);
     ct.showStatus();
 
+    ScavTrap st("Scavvy");
+    st.attack("target");
+    st.guardGate();
+    st.takeDamage(30);
+    st.beRepaired(20);
+    st.showStatus();
+
+    return 0;
+}
+
+int main() {
+    // Teste de construção e destruição
+    {
+        std::cout << "Creating ClapTrap ct(\"Clappy\"):" << std::endl;
+        ClapTrap ct("Clappy");
+        std::cout << "Creating ScavTrap st(\"Scavvy\"):" << std::endl;
+        ScavTrap st("Scavvy");
+    } // Aqui os destruidores serão chamados automaticamente
+
+    std::cout << std::endl;
+
+    // Testando funcionalidades do ClapTrap
+    ClapTrap ct("Clappy");
+    ct.attack("target");
+    ct.takeDamage(5);
+    ct.beRepaired(3);
+    ct.showStatus();
+
+    std::cout << std::endl;
+
+    // Testando funcionalidades do ScavTrap
     ScavTrap st("Scavvy");
     st.attack("target");
     st.guardGate();
