a0f1ee55c46ece34d4da1c0b9a56cb8379905289
gsmereka <gsmereka200@gmail.com>
2024-05-25 19:58:28 -0300

    update-main-tests

--- cpp04/ex02/sources/main.cpp
+++ cpp04/ex02/sources/main.cpp
@@ -16,14 +16,14 @@ int test_animalsound(void)
     return (0);
 }
 
-int main(void)
+int main()
 {
     const int arraySize = 10;
     Animal* animals[arraySize];
 
-    std::cout << "------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
     std::cout << "Creating half Dog and half Cat objects" << std::endl;
-    std::cout << "------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
     for (int i = 0; i < arraySize / 2; ++i)
     {
         animals[i] = new Dog();
@@ -33,18 +33,26 @@ int main(void)
         animals[i] = new Cat();
     }
 
-    std::cout << "------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
     std::cout << "Demonstrating polymorphism and type checking" << std::endl;
-    std::cout << "------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
     for (int i = 0; i < arraySize; ++i)
     {
         std::cout << animals[i]->getType() << " ";
         animals[i]->makeSound();
     }
 
-    std::cout << "------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Testing deep copy" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    Dog basic;
+    {
+        Dog tmp = basic;
+    }
+
+    std::cout << "------------------------------" << std::endl;
     std::cout << "Cleaning up" << std::endl;
-    std::cout << "------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
     for (int i = 0; i < arraySize; ++i)
     {
         delete animals[i];
