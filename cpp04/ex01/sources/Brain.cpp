2777e340ae5653419d99da70aa496afc6edafdbf
gsmereka <gsmereka200@gmail.com>
2024-05-25 19:57:29 -0300

    update-brain-copy

--- cpp04/ex01/sources/Brain.cpp
+++ cpp04/ex01/sources/Brain.cpp
@@ -13,12 +13,14 @@ Brain::~Brain()
 
 Brain::Brain(const Brain &other)
 {
-    *this = other;
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = other.ideas[i];
     std::cout << "Brain copied!" << std::endl;
 }
 
 Brain& Brain::operator=(const Brain &other)
 {
+    std::cout << "Assignation with operator= called" <<std::endl;
     if (this != &other)
     {
         for (int i = 0; i < 100; ++i)
--- cpp04/ex01/sources/main.cpp
+++ cpp04/ex01/sources/main.cpp
@@ -3,9 +3,52 @@
 #include "../headers/Cat.hpp"
 #include <iostream>
 
+int main()
+{
+    const int arraySize = 10;
+    Animal* animals[arraySize];
 
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Creating half Dog and half Cat objects" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    for (int i = 0; i < arraySize / 2; ++i)
+    {
+        animals[i] = new Dog();
+    }
+    for (int i = arraySize / 2; i < arraySize; ++i)
+    {
+        animals[i] = new Cat();
+    }
 
-int main(void)
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Demonstrating polymorphism and type checking" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    for (int i = 0; i < arraySize; ++i)
+    {
+        std::cout << animals[i]->getType() << " ";
+        animals[i]->makeSound();
+    }
+
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Testing deep copy" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    Dog basic;
+    {
+        Dog tmp = basic;
+    } // Destructor of tmp should not delete the Brain of basic
+
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Cleaning up" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    for (int i = 0; i < arraySize; ++i)
+    {
+        delete animals[i];
+    }
+
+    return 0;
+}
+
+int main2(void)
 {
     const int arraySize = 10;
     Animal* animals[arraySize];
