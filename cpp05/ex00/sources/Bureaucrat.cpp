eab2253cb36d1e376f6af930ad5478691a357a33
gsmereka <gsmereka200@gmail.com>
2024-05-28 14:58:05 -0300

    att-bureaucrat-msg

--- cpp05/ex00/sources/Bureaucrat.cpp
+++ cpp05/ex00/sources/Bureaucrat.cpp
@@ -88,10 +88,10 @@ std::ostream& operator<<(std::ostream &os, const Bureaucrat &b)
 // exceptions
 const char *Bureaucrat::GradeTooHighException::what() const throw()
 {
-	return "Form grade is too high!";
+	return "Bureaucrat grade is too high!";
 }
 
 const char *Bureaucrat::GradeTooLowException::what() const throw()
 {
-	return "Form grade is too low!";
+	return "Bureaucrat grade is too low!";
 }
