6dced843acc3145311242766198176d884ab956d
gsmereka <gsmereka200@gmail.com>
2024-05-28 14:49:19 -0300

    att-main-tests

--- cpp05/ex00/sources/main.cpp
+++ cpp05/ex00/sources/main.cpp
@@ -1,35 +1,65 @@
 #include "../headers/Bureaucrat.hpp"
 #include <iostream>
 
-static void testFailToSignForm();
+static void testFailToCreateBureaucrat();
+static void testSucessToCreateBureaucrat();
 static void testIncrementGradeFail();
+static void testDecrementGradeFail();
 
 int main(void)
 {
-	testFailToSignForm();
+	testFailToCreateBureaucrat();
+	testSucessToCreateBureaucrat();
 	testIncrementGradeFail();
-	return 0;
+	testDecrementGradeFail();
+	return (0);
 }
 
-static void testFailToSignForm()
+static void testFailToCreateBureaucrat()
 {
 	std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
-	std::cout << "Attempting to sign a Form, but fail:" << std::endl;
+	std::cout << "⚠️  Attempting to create a Bureaucrat, but fail:  ⚠️" << std::endl;
 	std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
-	Bureaucrat bob("Bob", 100);
+	
+	try
+	{
+		std::cout << "The next line should throw an exception:" << std::endl << std::endl;
+		Bureaucrat bob("Bob", 0);
 
-	std::cout << bob << std::endl;
-
-	std::cout << "The next line should throw an exception:" << std::endl << std::endl;
-	// bob.signForm(formA);
+		std::cout << bob << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl << std::endl;
+	}
 }
+
+static void testSucessToCreateBureaucrat()
+{
+	std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
+	std::cout << " ⚠️  Attempting to create a Bureaucrat: ⚠️" << std::endl;
+	std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
+	
+	try
+	{
+		std::cout << "The next line should throw an exception:" << std::endl << std::endl;
+		Bureaucrat bob("Bob", 1);
+
+		std::cout << bob << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl << std::endl;
+	}
+}
+
 
 static void testIncrementGradeFail()
 {
 	try
 	{
 		std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
-		std::cout << "Attempting to increment a grade, but fail:" << std::endl;
+		std::cout << " ⚠️  Attempting to increment a grade, but fail: ⚠️" << std::endl;
 		std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
 		Bureaucrat bob("Bob", 1);
 
@@ -37,7 +67,28 @@ static void testIncrementGradeFail()
 
 		std::cout << "The next line should throw an exception:" << std::endl << std::endl;
 		bob.incrementGrade();
-		// bob.signForm(formA);
+		std::cout << bob << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl << std::endl;
+	}
+}
+
+static void testDecrementGradeFail()
+{
+	try
+	{
+		std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
+		std::cout << "⚠️  Attempting to decrement a grade, but fail:  ⚠️" << std::endl;
+		std::cout << "\n------------------------------------------------------------------------------\n" << std::endl;
+		Bureaucrat bob("Bob", 150);
+
+		std::cout << bob << std::endl;
+
+		std::cout << "The next line should throw an exception:" << std::endl << std::endl;
+		bob.decrementGrade();
+		std::cout << bob << std::endl;
 	}
 	catch (const std::exception &e)
 	{
