63eecde918104e69f8407b9601505c41c7cf1a59
gsmereka <gsmereka200@gmail.com>
2024-06-03 18:38:06 -0300

    att-main

--- cpp06/ex00/sources/main.cpp
+++ cpp06/ex00/sources/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: " << argv[0] << " <literal>" << std::endl;
+        std::cerr << "Usage: ./scalarConverter <literal>" << std::endl;
         return (1);
     }
 
