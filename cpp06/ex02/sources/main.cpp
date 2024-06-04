6359ad441a33b452d265024cdbedb8cdd83a7bbe
gsmereka <gsmereka200@gmail.com>
2024-06-04 17:05:26 -0300

    att main

--- cpp06/ex02/sources/main.cpp
+++ cpp06/ex02/sources/main.cpp
@@ -1,8 +1,11 @@
 #include "../headers/Base.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
-extern Base* generate(void);
-extern void identify(Base *p);
-extern void identify(Base &p);
+Base *generate(void);
+void identify(Base *p);
+void identify(Base &p);
 
 int main(void)
 {
@@ -13,4 +16,69 @@ int main(void)
 
     delete obj;
     return 0;
+}
+
+void identify(Base *p)
+{
+    if (dynamic_cast<A*>(p))
+    {
+        std::cout << "A" << std::endl;
+    }
+    else if (dynamic_cast<B*>(p))
+    {
+        std::cout << "B" << std::endl;
+    }
+    else if (dynamic_cast<C*>(p))
+    {
+        std::cout << "C" << std::endl;
+    }
+    else
+    {
+        std::cout << "Unknown type" << std::endl;
+    }
+}
+
+
+void identify(Base &p)
+{
+	try
+	{
+		A a = dynamic_cast<A&>(p);
+		std::cout <<  "A" << std::endl;
+		return ;
+	}	catch(...) {}
+	try
+	{
+		B b = dynamic_cast<B&>(p);
+		std::cout <<  "B" << std::endl;
+		return ;
+	}	catch(...) {}
+	try
+	{
+		C c = dynamic_cast<C&>(p);
+		std::cout <<  "C" << std::endl;
+		return ;
+	}
+	catch (...) 
+	{
+		std::cout << "This class is not in my list of valid classes!\n";
+	}
+}
+
+Base* generate(void)
+{
+    srand(time(0));
+    int choice = rand() % 3;
+
+    switch (choice)
+    {
+        case 0:
+            return new A();
+        case 1:
+            return new B();
+        case 2:
+            return new C();
+        default:
+            return NULL;
+    }
 }
