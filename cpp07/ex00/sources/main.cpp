4e6b194c49ab20425b8bb0c17a5e60255ed445cc
gsmereka <gsmereka200@gmail.com>
2024-06-04 21:29:44 -0300

    att0main

--- cpp07/ex00/sources/main.cpp
+++ cpp07/ex00/sources/main.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
-#include "../headers/swap_min_max.hpp"
+#include "../headers/SwapMinMax.hpp"
 
-int main()
+int main(void)
 {
-    int a = 2;
-    int b = 3;
-    ::swap(a, b);
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+	int a = 2;
+	int b = 3;
 
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
-    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	std::cout << "Before Swap " << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap(a, b);
+	std::cout << "After Swap " << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 
-    return 0;
+	std::cout << "\n------------------------------------------------------------\n" << std::endl;
+
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::cout << "\n------------------------------------------------------------\n" << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	std::cout << "Before Swap " << std::endl;
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	::swap(c, d);
+	std::cout << "After Swap " << std::endl;
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+
+	std::cout << "\n------------------------------------------------------------\n" << std::endl;
+
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+	return 0;
 }
