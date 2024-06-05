105b05743adf3d25c26055b5d32c6e5ea05c5a26
gsmereka <gsmereka200@gmail.com>
2024-06-05 20:22:39 -0300

    att-main

--- cpp08/ex01/sources/Span.cpp
+++ cpp08/ex01/sources/Span.cpp
@@ -3,52 +3,52 @@
 
 Span::Span(int size) : maxSize(size)
 {
-    numbers.reserve(size);
+	numbers.reserve(size);
 }
 
 Span::~Span() {}
 
 void Span::addNumber(int number)
 {
-    if (static_cast<int>(numbers.size()) >= maxSize)
-    {
-        throw std::out_of_range("Span is full");
-    }
-    numbers.push_back(number);
+	if (static_cast<int>(numbers.size()) >= maxSize)
+	{
+		throw std::out_of_range("Span is full");
+	}
+	numbers.push_back(number);
 }
 
 int	Span::shortestSpan(void)
 {
-    if (numbers.size() < 2)
-    {
-        throw std::out_of_range("Cannot find span with less than 2 numbers");
-    }
+	if (numbers.size() < 2)
+	{
+		throw std::out_of_range("Cannot find span with less than 2 numbers");
+	}
 
-    std::vector<int> sortedNumbers = numbers;
-    std::sort(sortedNumbers.begin(), sortedNumbers.end());
+	std::vector<int> sortedNumbers = numbers;
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
 
-    int minSpan = sortedNumbers[1] - sortedNumbers[0];
-    for (size_t i = 2; i < sortedNumbers.size(); ++i)
-    {
-        int span = sortedNumbers[i] - sortedNumbers[i - 1];
-        if (span < minSpan)
-        {
-            minSpan = span;
-        }
-    }
+	int minSpan = sortedNumbers[1] - sortedNumbers[0];
+	for (size_t i = 2; i < sortedNumbers.size(); ++i)
+	{
+		int span = sortedNumbers[i] - sortedNumbers[i - 1];
+		if (span < minSpan)
+		{
+			minSpan = span;
+		}
+	}
 
-    return minSpan;
+	return minSpan;
 }
 
 int Span::longestSpan()
 {
-    if (numbers.size() < 2)
-    {
-        throw std::out_of_range("Cannot find span with less than 2 numbers");
-    }
+	if (numbers.size() < 2)
+	{
+		throw std::out_of_range("Cannot find span with less than 2 numbers");
+	}
 
-    int min = *std::min_element(numbers.begin(), numbers.end());
-    int max = *std::max_element(numbers.begin(), numbers.end());
+	int min = *std::min_element(numbers.begin(), numbers.end());
+	int max = *std::max_element(numbers.begin(), numbers.end());
 
-    return max - min;
+	return max - min;
 }
--- cpp08/ex01/sources/main.cpp
+++ cpp08/ex01/sources/main.cpp
@@ -4,16 +4,16 @@
 void printHeader(std::string str)
 {
 	std::cout << std::endl << std::string(42, '=') << std::endl
-            << str
-            << std::endl
-            << std::string(42, '=') << std::endl;
+			<< str
+			<< std::endl
+			<< std::string(42, '=') << std::endl;
 }
 
-int main()
+int main(void)
 {
 	printHeader("TEST: PDF");
 	try
-    {
+	{
 		Span	sp = Span(5);
 
 		sp.addNumber(6);
@@ -26,13 +26,13 @@ int main()
 		std::cout << sp.longestSpan() << std::endl;
 	}
 	catch (const std::out_of_range &e)
-    {
+	{
 		std::cout << "Erro: " << e.what() << std::endl;
 	}
 
 	printHeader("TEST: REPEATED NUMBERS");
-    try
-    {
+	try
+	{
 		Span	sp = Span(8);
 
 		sp.addNumber(0);
@@ -42,42 +42,42 @@ int main()
 		std::cout << sp.longestSpan() << std::endl;
 	}
 	catch (const std::out_of_range &e)
-    {
+	{
 		std::cout << "Erro: " << e.what() << std::endl;
 	}
 	
-    printHeader("TEST: NEGATIVE VALUE");
+	printHeader("TEST: NEGATIVE VALUE");
 	try
-    {
+	{
 		Span sp = Span(5);
 
 		sp.addNumber(0);
 		sp.addNumber(-42);
-        sp.addNumber(42);
+		sp.addNumber(42);
 		std::cout << sp.shortestSpan() << std::endl;
-        std::cout << sp.longestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
 	}
 	catch (const std::out_of_range& e)
-    {
+	{
 		std::cout << "Erro: " << e.what() << std::endl;
 	}
 
 	printHeader("TEST: MANY NUMBERS");
 	try
-    {
-        Span sp = Span(42000);
-        for (int i = 0; i < 42000; i++)
+	{
+		Span sp = Span(42000);
+		for (int i = 0; i < 42000; i++)
 			sp.addNumber(i);
 		std::cout << sp.longestSpan() << std::endl;
 		std::cout << sp.shortestSpan() << std::endl;
 	}
 	catch (const std::out_of_range& e)
-    {
+	{
 		std::cout << "Erro: " << e.what() << std::endl;
 	}
 	printHeader("TEST: EXCEEDED QUANTITY");
 	try
-    {
+	{
 		Span sp = Span(5);
 
 		sp.addNumber(30);
@@ -90,36 +90,22 @@ int main()
 		std::cout << sp.longestSpan() << std::endl;
 	}
 	catch (const std::out_of_range& e)
-    {
+	{
 		std::cout << "Erro: " << e.what() << std::endl;
 	}
 
 	printHeader("TEST: EXCEEDED QUANTITY");
 	try
-    {
+	{
 		Span sp = Span(10000);
 		for (int i = 0; i < 20000; i++)
 			sp.addNumber(i + 2);
 		std::cout << sp.shortestSpan() << std::endl;
-        std::cout << sp.longestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
 	}
 	catch (const std::out_of_range& e)
-    {
+	{
 		std::cout << "Erro: " << e.what() << std::endl;
 	}
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// ordena o conteiner ordenado 
-// passa uma vez a diferenca dele pro proximo 
