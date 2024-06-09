307f6654423b1ebd8646eed8f81e3fdd897b272f
gsmereka <gsmereka200@gmail.com>
2024-06-09 16:35:22 -0300

    att

--- cpp08/ex02/sources/main.cpp
+++ cpp08/ex02/sources/main.cpp
@@ -1,37 +1,37 @@
 #include  "../headers/MutantStack.hpp"
 #include  <deque>
-#include  <list>
 
-int main() {
-    MutantStack<int> mstack;
+int main(void)
+{
+	MutantStack<int> mstack;
 
-    mstack.push(1);
-    mstack.push(2);
-    mstack.push(3);
+	mstack.push(1);
+	mstack.push(2);
+	mstack.push(3);
 
-    std::cout << "Elements on stack: ";
-    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
+	std::cout << "Elements on stack: ";
+	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
 	{
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 
-    mstack.push(4);
-    mstack.push(5);
-    mstack.push(6);
+	mstack.push(4);
+	mstack.push(5);
+	mstack.push(6);
 
-    std::cout << "Elements on stack: ";
-    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
+	std::cout << "Elements on stack: ";
+	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
 	{
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 
-    std::cout << "Elements on stack: ";
-    for (MutantStack<int>::iterator it = mstack.end(); it != mstack.begin(); --it)
+	std::cout << "Elements on stack: ";
+	for (MutantStack<int>::iterator it = mstack.end(); it != mstack.begin(); --it)
 	{
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-    return 0;
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
