c99b61bdc1ecca2664f5f1c71a371824c495915f
gsmereka <gsmereka200@gmail.com>
2024-06-09 16:52:21 -0300

    att-main

--- cpp08/ex02/headers/MutantStack.hpp
+++ cpp08/ex02/headers/MutantStack.hpp
@@ -11,14 +11,16 @@ class MutantStack : public std::stack<T, Container>
 {
 	public:
 		typedef typename Container::iterator iterator;
-
-		iterator begin() {
+		iterator begin()
+		{
 			return this->c.begin();
 		}
 
-		iterator end() {
+		iterator end()
+		{
 			return this->c.end();
 		}
+
 	};
 
 #endif
--- cpp08/ex02/sources/main.cpp
+++ cpp08/ex02/sources/main.cpp
@@ -1,37 +1,82 @@
 #include  "../headers/MutantStack.hpp"
 #include  <deque>
+#include <vector>
+#include <list>
 
 int main(void)
 {
-	MutantStack<int> mstack;
+	// DEQUE
+    std::cout << "Testing with default container (deque)" << std::endl;
+    MutantStack<int> mstack;
+    mstack.push(1);
+    mstack.push(2);
+    mstack.push(3);
 
-	mstack.push(1);
-	mstack.push(2);
-	mstack.push(3);
-
-	std::cout << "Elements on stack: ";
-	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
+    std::cout << "Elements in MutantStack: ";
+    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
 	{
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 
-	mstack.push(4);
-	mstack.push(5);
-	mstack.push(6);
-
-	std::cout << "Elements on stack: ";
-	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
+    mstack.pop();
+    std::cout << "After pop, elements in MutantStack: ";
+    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
 	{
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "------------------------------------------------\n" << std::endl;
 
-	std::cout << "Elements on stack: ";
-	for (MutantStack<int>::iterator it = mstack.end(); it != mstack.begin(); --it)
+
+
+	// LIST
+    std::cout << "Testing with std::list as container" << std::endl;
+    MutantStack<int, std::list<int> > mstack_list;
+    mstack_list.push(10);
+    mstack_list.push(20);
+    mstack_list.push(30);
+
+    std::cout << "Elements in MutantStack (list): ";
+    for (MutantStack<int, std::list<int> >::iterator it = mstack_list.begin(); it != mstack_list.end(); ++it)
 	{
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
-	return 0;
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    mstack_list.pop();
+    std::cout << "After pop, elements in MutantStack (list): ";
+    for (MutantStack<int, std::list<int> >::iterator it = mstack_list.begin(); it != mstack_list.end(); ++it)
+	{
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "------------------------------------------------\n" << std::endl;
+
+
+
+	// VECTOR
+    std::cout << "Testing with std::vector as container" << std::endl;
+    MutantStack<int, std::vector<int> > mstack_vector;
+    mstack_vector.push(100);
+    mstack_vector.push(200);
+    mstack_vector.push(300);
+
+    std::cout << "Elements in MutantStack (vector): ";
+    for (MutantStack<int, std::vector<int> >::iterator it = mstack_vector.begin(); it != mstack_vector.end(); ++it)
+	{
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    mstack_vector.pop();
+    std::cout << "After pop, elements in MutantStack (vector): ";
+    for (MutantStack<int, std::vector<int> >::iterator it = mstack_vector.begin(); it != mstack_vector.end(); ++it)
+	{
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "------------------------------------------------\n" << std::endl;
+    return 0;
 }
