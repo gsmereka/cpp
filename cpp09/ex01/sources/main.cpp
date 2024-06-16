fa476710f754c0a4fb7a583cc59e5f4420f867ae
gsmereka <gsmereka200@gmail.com>
2024-06-16 14:14:13 -0300

    att-main

--- cpp09/ex01/sources/main.cpp
+++ cpp09/ex01/sources/main.cpp
@@ -3,22 +3,22 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        std::cerr << "Usage: " << argv[0] << " \"<expression>\"" << std::endl;
-        return 1;
-    }
+	if (argc != 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " \"<expression>\"" << std::endl;
+		return 1;
+	}
 
-    RPN calculator;
-    try
-    {
-        double result = calculator.evaluate(argv[1]);
-        std::cout << result << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Error" << std::endl;
-    }
+	RPN calculator;
+	try
+	{
+		double result = calculator.evaluate(argv[1]);
+		std::cout << result << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error" << std::endl;
+	}
 
-    return 0;
+	return 0;
 }
